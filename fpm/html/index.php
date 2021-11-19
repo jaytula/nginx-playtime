cebaa067f021f1efb83e0b4e6afa33193d0f0df8
Jay T <jaytula@gmail.com>
2021-11-18 20:18:22 -0800

    Proxy Pass fpm

--- fpm/html/index.php
+++ fpm/html/index.php
@@ -1,5 +1,34 @@
+<!DOCTYPE html>
 <?php
-phpinfo();
+$url = "http://cra:3000/dist/asset-manifest.json";
+
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HEADER, false);
+
+$data = curl_exec($curl);
+
+curl_close($curl);
+
+$decoded = json_decode($data);
+$script_prefix = '/dist/';
 ?>
 
-<h1 style="text-align: center">Blah</h1>
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div id="root"></div>
+  
+  <?php foreach($decoded->entrypoints as $entry): ?>
+    <script src="<?= $script_prefix . $entry ?>"></script>
+  <?php endforeach; ?>
+</body>
+</html>
